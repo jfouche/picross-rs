f93e5c5338b242b4606961541c673a7adbece8c2
Jérémie Fouché <jeremie.fouche@gmail.com>
2022-01-04 12:11:07 +0100

    refactoring

--- src/game.rs
+++ src/game.rs
@@ -1,7 +1,9 @@
 use std::error;
 use std::fmt;
+use std::fmt::Display;
 use std::path::Path;
 
+use image::ImageBuffer;
 use image::{io::Reader as ImageReader, DynamicImage, DynamicImage::*, GenericImageView};
 
 #[derive(Debug)]
@@ -12,10 +14,16 @@ pub struct Game {
     pub cols: Vec<Vec<Clue>>,
 }
 
-#[derive(Debug,PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq)]
 pub struct Clue {
     pub color: image::Rgb<u8>,
     pub count: u32,
+}
+
+impl Display for Clue {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Clue ({:?}, {})", self.color, self.count)
+    }
 }
 
 impl Clue {
@@ -74,6 +82,67 @@ type Result<T> = std::result::Result<T, GameError>;
 
 const WHITE: image::Rgb<u8> = image::Rgb([0xFF, 0xFF, 0xFF]);
 
+fn is_white(p: &image::Rgb<u8>) -> bool {
+    return p[0] == WHITE[0] && p[1] == WHITE[1] && p[2] == WHITE[2];
+}
+
+struct Counter {
+    image: ImageBuffer<image::Rgb<u8>, Vec<u8>>,
+    counter: u32,
+    current_color: image::Rgb<u8>,
+}
+
+///
+impl Counter {
+    fn new(image: ImageBuffer<image::Rgb<u8>, Vec<u8>>) -> Self {
+        Counter {
+            image,
+            counter: 0,
+            current_color: WHITE,
+        }
+    }
+    fn reset(&mut self) {
+        self.counter = 0;
+        self.current_color = WHITE;
+    }
+    fn next(&mut self, x: u32, y: u32) -> Option<Clue> {
+        let pix = self.image.get_pixel(x, y);
+        if is_white(&pix) {
+            if self.counter > 0 {
+                let clue = self.clue();
+                // println!("Counter::next({},{}) return {}", x, y, clue);
+                self.reset();
+                return Some(clue);
+            }
+        } else {
+            if pix.eq(&self.current_color) {
+                self.counter += 1;
+                // println!("Counter::next({},{}) increment  {:?} : {}", x, y, self.current_color, self.counter);
+            } else {
+                self.current_color = *pix;
+                self.counter = 1;
+                // println!("Counter::next({},{}) initialize {:?} : {}", x, y, self.current_color, self.counter);
+            }
+        }
+        None
+    }
+
+    fn clue(&self) -> Clue {
+        Clue::new(self.current_color, self.counter)
+    }
+
+    fn end(&mut self) -> Option<Clue> {
+        if self.counter > 0 {
+            // println!("Counter::end() return {:?}", self.clue());
+            Some(self.clue())
+        }
+        else {
+            None
+        }
+    }
+}
+
+///
 impl Game {
     pub fn from_image<P>(filename: P) -> Result<Self>
     where
@@ -91,46 +160,23 @@ impl Game {
             }
         };
 
-        let is_white = |p: &image::Rgb<u8>| {
-            return p[0] == 0xFF && p[1] == 0xFF && p[2] == 0xFF;
-        };
+        let mut counter = Counter::new(imgbuffer);
 
         // Get the rows
         let mut rows = vec![];
         for y in 0..height {
-            let mut counter = 0;
-            let mut current_color = WHITE;
+            counter.reset();
             let mut v = vec![];
             for x in 0..width {
-                let pix = imgbuffer.get_pixel(x, y);
-                println!("Pixel[{}, {}] = {:?}", x, y, pix);
-
-                if is_white(&pix) {
-                    if counter > 0 {
-                        println!("   pushing {:?}", current_color);
-                        v.push(Clue {
-                            color: current_color,
-                            count: counter,
-                        });
-                        counter = 0;
-                        current_color = WHITE;
-                    }
-                } else {
-                    if pix.eq(&current_color) {
-                        counter += 1;
-                        println!("   incrementing {:?} : {}", current_color, counter);
-                    } else {
-                        current_color = *pix;
-                        counter = 1;
-                        println!("   initialize {:?} ", current_color);
-                    }
+                if let Some(clue) = counter.next(x, y) {
+                    // println!("===================================== pushing {:?}", clue);
+                    v.push(clue);
+                    counter.reset();
                 }
             }
-            if counter > 0 {
-                v.push(Clue {
-                    color: current_color,
-                    count: counter,
-                });
+            if let Some(clue) = counter.end() {
+                // println!("===================================== pushing {:?}", clue);
+                v.push(clue);
             }
             rows.push(v);
         }
@@ -138,39 +184,18 @@ impl Game {
         // Get the cols
         let mut cols = vec![];
         for x in 0..width {
-            let mut counter = 0;
-            let mut current_color = WHITE;
+            counter.reset();
             let mut v = vec![];
             for y in 0..height {
-                let pix = imgbuffer.get_pixel(x, y);
-                println!("Pixel[{}, {}] = {:?}", x, y, pix);
-
-                if is_white(&pix) {
-                    if counter > 0 {
-                        println!("   pushing {:?}", current_color);
-                        v.push(Clue {
-                            color: current_color,
-                            count: counter,
-                        });
-                        counter = 0;
-                        current_color = WHITE;
-                    }
-                } else {
-                    if pix.eq(&current_color) {
-                        counter += 1;
-                        println!("   incrementing {:?} : {}", current_color, counter);
-                    } else {
-                        current_color = *pix;
-                        counter = 1;
-                        println!("   initialize {:?} ", current_color);
-                    }
+                if let Some(clue) = counter.next(x, y) {
+                    // println!("===================================== pushing {:?}", clue);
+                    v.push(clue);
+                    counter.reset();
                 }
             }
-            if counter > 0 {
-                v.push(Clue {
-                    color: current_color,
-                    count: counter,
-                });
+            if let Some(clue) = counter.end() {
+                // println!("===================================== pushing {:?}", clue);
+                v.push(clue);
             }
             cols.push(v);
         }
