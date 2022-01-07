f78ee67b89af62c774b9af0879db20fac02187c7
Jérémie Fouché <jeremie.fouche@gmail.com>
2022-01-07 13:22:35 +0100

    better module/visibility managment

--- src/solver/full_line.rs
+++ src/solver/full_line.rs
@@ -1,9 +1,8 @@
+use crate::board::{Board, Pixel};
 use crate::game::Game;
-use crate::board::{Pixel, Board};
-use crate::picross_image::{Image, Clue, WHITE};
+use crate::picross_image::{Clue, Image, WHITE};
 
 use super::SolverAlgo;
-
 
 pub struct FullRow;
 
@@ -14,9 +13,9 @@ impl SolverAlgo for FullRow {
         let image = &game.image;
         let board = &mut game.board;
         for (y, row) in image.rows.iter().enumerate() {
-            let mut line = Row::new(image, board, y);
+            let mut line = row::Row::new(image, board, y);
             if solve_line(&mut line, row) {
-                return true
+                return true;
             }
         }
         false
@@ -28,9 +27,9 @@ impl SolverAlgo for FullCol {
         let image = &game.image;
         let board = &mut game.board;
         for (x, col) in image.cols.iter().enumerate() {
-            let mut line = Col::new(image, board, x);
+            let mut line = col::Col::new(image, board, x);
             if solve_line(&mut line, col) {
-                return true
+                return true;
             }
         }
         false
@@ -43,110 +42,120 @@ trait Line {
     fn set_pixel(&mut self, index: usize, pixel: Pixel);
 }
 
-struct Row<'a> {
-    image: &'a Image,
-    board: &'a mut Board,
-    nb_rows: usize
-}
-
-impl<'a> Row<'a> {
-    fn new(image: &'a Image, board: &'a mut Board, nb_rows: usize) -> Self {
-        Row {
-            image, 
-            board,
-            nb_rows
-        }
-    }
-}
-
-impl<'a> Line for Row<'a> {
-    fn size(&self) -> usize{
-        self.image.width as usize
-    }
-
-    fn get_pixel(&self, index: usize) -> &Pixel {
-        &self.board.get_pixel(index, self.nb_rows)
-    }
-
-    fn set_pixel(&mut self, index: usize, pixel: Pixel) {
-        self.board.set_pixel(index, self.nb_rows, &pixel);
-    }
-}
-
-struct Col<'a> {
-    image: &'a Image,
-    board: &'a mut Board,
-    nb_cols: usize
-}
-
-impl<'a> Col<'a> {
-    fn new(image: &'a Image, board: &'a mut Board, nb_cols: usize) -> Self {
-        Col {
-            image, 
-            board,
-            nb_cols
-        }
-    }
-}
-
-impl<'a> Line for Col<'a> {
-    fn size(&self) -> usize{
-        self.image.height as usize
-    }
-
-    fn get_pixel(&self, index: usize) -> &Pixel {
-        &self.board.get_pixel(self.nb_cols, index)
-    }
-
-    fn set_pixel(&mut self, index: usize, pixel: Pixel) {
-        self.board.set_pixel(self.nb_cols, index, &pixel);
-    }
-}
-
 fn solve_line(line: &mut dyn Line, clues: &Vec<Clue>) -> bool {
-        let mut current_color = WHITE;
-        let mut counter = 0 as usize;
+    let mut current_color = WHITE;
+    let mut counter = 0 as usize;
 
-        // count how many pixel are there can be if they are all collapsed
+    // count how many pixel are there can be if they are all collapsed
+    for clue in clues {
+        if clue.color.eq(&current_color) {
+            counter += 1;
+        }
+        counter += clue.count as usize;
+        current_color = clue.color;
+    }
+
+    if counter == line.size() {
+        // a full line is available
+        let mut current_color = WHITE;
+        let mut index = 0;
+        let mut modified = false;
         for clue in clues {
             if clue.color.eq(&current_color) {
-                counter += 1;
+                line.set_pixel(index, Pixel::Cross);
+                index += 1;
             }
-            counter += clue.count as usize;
-            current_color = clue.color;
-        }
-        
-        if counter == line.size() {
-            // a full line is available
-            let mut current_color = WHITE;
-            let mut index = 0;
-            let mut modified = false;
-            for clue in clues {
-                if clue.color.eq(&current_color) {
-                    line.set_pixel(index, Pixel::Cross);
-                    index += 1;
-                }
-                for _ in 0..clue.count {
-                    match line.get_pixel(index) {
-                        Pixel::Color(color) => {
-                            if color.eq(&WHITE) {
-                                modified = true;
-                            }
-                        }
-                        Pixel::Cross => {
+            for _ in 0..clue.count {
+                match line.get_pixel(index) {
+                    Pixel::Color(color) => {
+                        if color.eq(&WHITE) {
                             modified = true;
                         }
                     }
-                    line.set_pixel(index, Pixel::Color(clue.color));
-                    index += 1;
+                    Pixel::Cross => {
+                        modified = true;
+                    }
                 }
-                current_color = clue.color;
+                line.set_pixel(index, Pixel::Color(clue.color));
+                index += 1;
             }
-            if modified {
-                return true;
+            current_color = clue.color;
+        }
+        if modified {
+            return true;
+        }
+    }
+    false
+}
+
+mod row {
+    use super::Line;
+    use super::{Board, Image, Pixel};
+
+    pub struct Row<'a> {
+        image: &'a Image,
+        board: &'a mut Board,
+        nb_rows: usize,
+    }
+
+    impl<'a> Row<'a> {
+        pub fn new(image: &'a Image, board: &'a mut Board, nb_rows: usize) -> Self {
+            Row {
+                image,
+                board,
+                nb_rows,
             }
         }
-    false
+    }
+
+    impl<'a> Line for Row<'a> {
+        fn size(&self) -> usize {
+            self.image.width as usize
+        }
+
+        fn get_pixel(&self, index: usize) -> &Pixel {
+            &self.board.get_pixel(index, self.nb_rows)
+        }
+
+        fn set_pixel(&mut self, index: usize, pixel: Pixel) {
+            self.board.set_pixel(index, self.nb_rows, &pixel);
+        }
+    }
+}
+
+mod col {
+    use super::Line;
+    use super::{Board, Image, Pixel};
+
+    pub struct Col<'a> {
+        image: &'a Image,
+        board: &'a mut Board,
+        nb_cols: usize,
+    }
+
+    impl<'a> Col<'a> {
+        pub fn new(image: &'a Image, board: &'a mut Board, nb_cols: usize) -> Self {
+            Col {
+                image,
+                board,
+                nb_cols,
+            }
+        }
+    }
+
+    impl<'a> Line for Col<'a> {
+        fn size(&self) -> usize {
+            self.image.height as usize
+        }
+
+        fn get_pixel(&self, index: usize) -> &Pixel {
+            &self.board.get_pixel(self.nb_cols, index)
+        }
+
+        fn set_pixel(&mut self, index: usize, pixel: Pixel) {
+            self.board.set_pixel(self.nb_cols, index, &pixel);
+        }
+    }
 }
 
 #[cfg(test)]
@@ -164,15 +173,15 @@ mod tests {
         let game_res = Game::new("test/4x4-shuriken.png");
         assert!(&game_res.is_ok());
         let mut game = game_res.unwrap();
-        let solver = Solver { 
-            algos: vec![Box::new(FullRow {})]
+        let solver = Solver {
+            algos: vec![Box::new(FullRow {})],
         };
         assert!(solver.solve(&mut game));
         assert!(solver.solve(&mut game));
 
         // ██X█
-        //     
-        //     
+        //
+        //
         // █X██
 
         assert_eq!(game.board.get_pixel(0, 0), &BLACK);
@@ -191,8 +200,8 @@ mod tests {
         let game_res = Game::new("test/4x4-shuriken.png");
         assert!(&game_res.is_ok());
         let mut game = game_res.unwrap();
-        let solver = Solver { 
-            algos: vec![Box::new(FullCol {})]
+        let solver = Solver {
+            algos: vec![Box::new(FullCol {})],
         };
         assert!(solver.solve(&mut game));
         assert!(solver.solve(&mut game));
